cc9b84944851d786f9a5b8c53eaa437549c8c6bc
kristenmazza <kbmazza@gmail.com>
2022-12-19 12:06:23 -0800

    Play a game
    This commit will allow the user to play a game of five rounds upon loading their browser in the console.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    generatedNumber = Math.floor((Math.random()*30)+1);
-    if (generatedNumber <= 10) {
+    const generatedNumber = Math.floor((Math.random()*3)+1);
+    if (generatedNumber === 1) {
         generatedChoice = "rock";
-    } else if (generatedNumber <=20) {
+    } else if (generatedNumber === 2) {
         generatedChoice = "paper";
     } else {
         generatedChoice = "scissors";
@@ -11,51 +14,68 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    playerChoice = prompt("Choose rock, paper, or scissors", "");
+    let playerChoice = prompt("Choose rock, paper, or scissors", "");
     playerChoice = playerChoice.toLowerCase();
     while ((playerChoice != "rock") && (playerChoice != "paper") && (playerChoice != "scissors")) {
         playerChoice = prompt(`"${playerChoice}" is not valid. Please pick rock, paper, or scissors.`)
-        return playerChoice;
+        playerChoice = playerChoice.toLowerCase();
     }
     return playerChoice;
 }
 
+function playRound(computerSelection, playerSelection) {
 
-function playRound(playerSelection, computerSelection) {
-
-    console.log(playerSelection);
-    console.log(computerSelection);
-    
     if (playerSelection === "rock") {
         if (computerSelection === "paper") { 
-            console.log("You lose. Paper beats rock.");
+            computerScore++;
+            return "You lose. Paper beats rock.";
         } else if (computerSelection === "scissors") {
-            console.log("You win. Rock beats scissors.");
+            playerScore++;
+            return "You win. Rock beats scissors.";
         } else { 
-            console.log("It's a tie!");
+            return "It's a tie! You both selected rock.";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") { 
-            console.log("You win. Paper beats rock.");
+            playerScore++;
+            return "You win. Paper beats rock.";
         } else if (computerSelection === "scissors") {
-            console.log("You lose. Scissors beats paper.");
+            computerScore++;
+            return "You lose. Scissors beats paper.";
         } else { 
-            console.log("It's a tie!");
+            return "It's a tie! You both selected paper.";
         }
     } else { 
         if (computerSelection === "rock") { 
-            console.log("You lose. Rock beats scissors.");
+            computerScore++;
+            return "You lose. Rock beats scissors.";
         } else if (computerSelection === "paper") {
-            console.log("You win. Scissors beats paper.");
+            playerScore++;
+            return "You win. Scissors beats paper.";
         } else { 
-            console.log("It's a tie!");
+            return "It's a tie! You both selected scissors.";
         }
     }
-
   }
-   
 
 
-  const computerSelection = getComputerChoice();
-  const playerSelection = getPlayerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+function game() {
+
+    for (let round = 0; round < 5; round++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+        const roundResult = playRound(computerSelection, playerSelection);
+        console.log(roundResult);
+    }
+
+    if (computerScore > playerScore) {
+        console.log(`You lose. Your score is: ${playerScore}. Computer\'s score is: ${computerScore}.`);
+    } else if (playerScore > computerScore) {
+        console.log(`You win. Your score is: ${playerScore}. Computer\'s score is: ${computerScore}.`);
+    } else {
+        console.log(`It's a tie. Your score is: ${playerScore}. Computer\'s score is: ${computerScore}.`);
+    }
+
+}
+
+game();
